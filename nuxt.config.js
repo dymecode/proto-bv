846182006cedc2506b94cb5c7cb682e788cf8009
Darren Ee <darren.ee@dymecode.com>
2021-02-05 03:02:13 +0800

    add nuxt mods for fa and gfont

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,32 +6,27 @@ export default {
   head: {
     title: 'proto-bv',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: ['@nuxtjs/fontawesome', '@nuxtjs/google-fonts'],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
@@ -40,6 +35,18 @@ export default {
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
+
+  fontawesome: {
+    icons: {
+      solid: ['fas'],
+    },
+  },
+
+  googleFonts: {
+    families: {
+      Roboto: true,
+      Lato: [100, 300],
+    },
+  },
 }
