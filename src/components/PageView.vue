c226e2429092d49a7db98c6eb458ebaebe1227ca
Darren Ee <darren.ee@dymecode.com>
2021-02-07 11:48:56 +0800

    Migrate to vanilla vue/cli

---
+++ src/components/PageView.vue
@@ -0,0 +1,13 @@
+<template functional>
+  <div class="container-fluid">
+    <div class="row flex-xl-nowrap2">
+      <div class="bd-sidebar border-bottom-0 col-md-3 col-xl-2 col-12"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageView"
+};
+</script>
---
+++ src/layouts/imr/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="imr">
+    <navbar></navbar>
+    <page-view></page-view>
+  </div>
+</template>
+
+<script>
+
+import Navbar from '../../components/Navbar.vue';
+import PageView from '../../components/PageView.vue';
+export default {
+  name: "Main",
+  components: { Navbar, PageView }
+};
+</script>
