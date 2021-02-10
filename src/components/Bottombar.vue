229e44de522bfe1de27145edd9192bafaa5f910e
Darren Ee <darren.ee@dymecode.com>
2021-02-11 02:56:00 +0800

    create Bottombar.vue and start filters;

---
+++ src/components/Bottombar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bottombar">
+    <div class="bar-item">{{ item1 | implodeArray }}</div>
+    <div class="bar-item">{{ item2 | implodeArray }}</div>
+  </div>
+</template>
+
+<script>
+const acron = require("acron");
+export default {
+  name: "Bottombar",
+  computed: {
+    item1: function () {
+      return acron("omg");
+    },
+    item2: function () {
+      return acron("lesms");
+    }
+  }
+}
+</script>
--- src/layouts/imr/Main.vue
+++ src/layouts/imr/Main.vue
@@ -2,15 +2,17 @@
   <div class="imr">
     <navbar></navbar>
     <page-view></page-view>
+    <bottombar></bottombar>
   </div>
 </template>
 
 <script>
 import Navbar from "../../components/Navbar.vue";
 import PageView from "../../components/PageView.vue";
+import Bottombar from "../../components/Bottombar.vue";
 
 export default {
   name: "Main",
-  components: { Navbar, PageView }
+  components: { Navbar, PageView, Bottombar }
 };
 </script>
