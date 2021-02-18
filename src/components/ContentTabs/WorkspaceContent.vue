4298bfae73395aa7db04772d6761ae6d5f8dfa1e
Darren Ee <darren.ee@dymecode.com>
2021-02-19 03:39:12 +0800

    found the custom thead slot;

--- src/components/ContentTabs/WorkspaceContent.vue
+++ src/components/ContentTabs/WorkspaceContent.vue
@@ -2,30 +2,27 @@
   <b-container fluid>
     <!-- User Interface controls -->
     <b-row>
-
-
       <b-col class="my-1" lg="6">
         <b-form-group
-            class="mb-0"
-            label="Filter"
-            label-align-sm="right"
-            label-cols-sm="3"
-            label-for="filter-input"
-            label-size="sm"
+          class="mb-0"
+          label="Filter"
+          label-align-sm="right"
+          label-cols-sm="3"
+          label-for="filter-input"
+          label-size="sm"
         >
           <b-input-group size="sm">
             <b-form-input
-                id="filter-input"
-                v-model="filterInput"
-                placeholder="Type to Search"
-                type="search"
+              id="filter-input"
+              v-model="filterInput"
+              placeholder="Type to Search"
+              type="search"
             ></b-form-input>
 
             <b-input-group-append>
               <b-button :disabled="!filterInput" @click="filterInput = ''"
-              >Clear
-              </b-button
-              >
+                >Clear
+              </b-button>
             </b-input-group-append>
           </b-input-group>
         </b-form-group>
@@ -33,19 +30,19 @@
 
       <b-col class="my-1" lg="6">
         <b-form-group
-            v-slot="{ ariaDescribedby }"
-            v-model="sortDirection"
-            class="mb-0"
-            description="Leave all unchecked to filter on all data"
-            label="Filter On"
-            label-align-sm="right"
-            label-cols-sm="3"
-            label-size="sm"
+          v-slot="{ ariaDescribedby }"
+          v-model="sortDirection"
+          class="mb-0"
+          description="Leave all unchecked to filter on all data"
+          label="Filter On"
+          label-align-sm="right"
+          label-cols-sm="3"
+          label-size="sm"
         >
           <b-form-checkbox-group
-              v-model="filterOn"
-              :aria-describedby="ariaDescribedby"
-              class="mt-1"
+            v-model="filterOn"
+            :aria-describedby="ariaDescribedby"
+            class="mt-1"
           >
             <b-form-checkbox value="name">Name</b-form-checkbox>
             <b-form-checkbox value="age">Age</b-form-checkbox>
@@ -53,41 +50,51 @@
           </b-form-checkbox-group>
         </b-form-group>
       </b-col>
-
-
     </b-row>
 
     <!-- Main table element -->
     <b-table
-        :current-page="currentPage"
-        :fields="fields"
-        :filter="filterInput"
-        :filter-included-fields="filterOn"
-        :items="items"
-        :per-page="perPage"
-        :sort-by.sync="sortBy"
-        :sort-desc.sync="sortDesc"
-        :sort-direction="sortDirection"
-        show-empty
-        small
-        stacked="md"
-        @filtered="onFiltered"
+      :current-page="currentPage"
+      :fields="xfields"
+      :filter="filterInput"
+      :filter-included-fields="filterOn"
+      :items="xitems"
+      :per-page="perPage"
+      responsive
+      :sort-by.sync="sortBy"
+      :sort-desc.sync="sortDesc"
+      :sort-direction="sortDirection"
+      show-empty
+      sticky-header="30rem"
+      fixed
+      foot-clone
+      foot-variant="danger"
+      head-row-variant="info"
+      hover
+      :id="id"
+      no-border-collapse
+      no-sort-reset
+      outlined
+      :primary-key="id"
+      striped
+      @filtered="onFiltered"
     >
-      <template #cell(name)="row">
-        {{ row.value.first }} {{ row.value.last }}
-      </template>
-
       <template #cell(actions)="row">
-        <b-button
-            class="mr-1"
-            size="sm"
-            @click="info(row.item, row.index, $event.target)"
+        <a
+          @click="info(row.item, row.index, $event.target)"
+          class="btn btn-link"
         >
-          Info modal
-        </b-button>
-        <b-button size="sm" @click="row.toggleDetails">
-          {{ row.detailsShowing ? "Hide" : "Show" }} Details
-        </b-button>
+          <fa-icon icon="eye" size="lg" class="mr-1"></fa-icon>
+        </a>
+        <a @click="row.toggleDetails" class="btn btn-link">
+          <fa-icon
+            icon="save"
+            size="lg"
+            class="mr-1"
+            v-if="row.detailsShowing"
+          ></fa-icon>
+          <fa-icon icon="edit" size="lg" class="mr-1" v-else></fa-icon>
+        </a>
       </template>
 
       <template #row-details="row">
@@ -99,56 +106,107 @@
           </ul>
         </b-card>
       </template>
+
+      <template #bottom-row>
+        <b-td>{{ xfields }}</b-td>
+        <b-td>{{ columns }}</b-td>
+      </template>
+
+      <template #head(cat)>
+        <b-button size="sm">hi</b-button>
+      </template>
+
+      <template #thead-top>
+        <b-tr>
+          <b-th></b-th>
+          <b-td colspan="3">this is this</b-td>
+          <b-td colspan="4">that is that</b-td>
+        </b-tr>
+        <b-tr>
+          <template v-for="(col, index) in xfields">
+            <b-td :key="index">
+              <b-dropdown :id="'dropdown-form-' + index" class="m-2">
+                <template #button-content>
+                  <fa-icon icon="filter" size="lg"></fa-icon>
+                </template>
+                <b-dropdown-form>
+                  <b-form-group
+                    label="Email"
+                    label-for="dropdown-form-email"
+                    @submit.stop.prevent
+                  >
+                    <b-form-input
+                      id="dropdown-form-email"
+                      size="sm"
+                      placeholder="email@example.com"
+                    ></b-form-input>
+                  </b-form-group>
+
+                  <b-form-group
+                    label="Password"
+                    label-for="dropdown-form-password"
+                  >
+                    <b-form-input
+                      id="dropdown-form-password"
+                      type="password"
+                      size="sm"
+                      placeholder="Password"
+                    ></b-form-input>
+                  </b-form-group>
+
+                  <b-form-checkbox class="mb-3">Remember me</b-form-checkbox>
+                  <b-button variant="primary" size="sm" @click="onClick"
+                    >Sign In</b-button
+                  >
+                </b-dropdown-form>
+              </b-dropdown>
+            </b-td>
+          </template>
+        </b-tr>
+      </template>
     </b-table>
 
     <div class="level">
       <div class="level-left">
         <div class="level-item">
-          <b-input-group
-              append="per page"
-              class="mb-0"
-              size="sm"
-          >
+          <b-input-group append="per page" class="mb-0" size="sm">
             <b-form-select
-                id="per-page-select"
-                v-model="perPage"
-                :options="pageOptions"
-                size="sm"
+              id="per-page-select"
+              v-model="perPage"
+              :options="pageOptions"
+              size="sm"
             ></b-form-select>
           </b-input-group>
         </div>
-
       </div>
       <div class="level-right">
         <div class="level-item">
           <b-pagination
-              v-model="currentPage"
-              :per-page="perPage"
-              :total-rows="totalRows"
-              align="right"
-              class="my-0"
-              size="sm"
+            v-model="currentPage"
+            :per-page="perPage"
+            :total-rows="totalRows"
+            align="right"
+            class="my-0"
+            size="sm"
           ></b-pagination>
         </div>
       </div>
-
-
     </div>
 
     <!-- Info modal -->
     <b-modal
-        :id="infoModal.id"
-        :title="infoModal.title"
-        ok-only
-        @hide="resetInfoModal"
+      :id="infoModal.id"
+      :title="infoModal.title"
+      ok-only
+      @hide="resetInfoModal"
     >
       <pre>{{ infoModal.content }}</pre>
     </b-modal>
 
     <b-modal
-        id="workspace-filters"
-        title="workspace-filters"
-        @hide="resetFilterModal"
+      id="workspace-filters"
+      title="workspace-filters"
+      @hide="resetFilterModal"
     >
       <pre>{{ filterModal.content }}</pre>
     </b-modal>
@@ -156,6 +214,8 @@
 </template>
 
 <script>
+import fieldSpec from "../../schema/fields.json";
+import mockData from "../../schema/MOCK_DATA.json";
 export default {
   name: "WorkspaceContent",
   data() {
@@ -217,11 +277,11 @@ export default {
         },
         { key: "actions", label: "Actions" },
       ],
-      totalRows: 1,
+
       currentPage: 1,
       perPage: 5,
-      pageOptions: [ 5, 10, 15, { value: 100, text: "Show a lot" } ],
-      sortBy: "",
+      pageOptions: [5, 10, 15, { value: 100, text: "Show a lot" }],
+      sortBy: "dts",
       sortDesc: false,
       sortDirection: "asc",
       filterInput: null,
@@ -236,16 +296,21 @@ export default {
         title: "",
         content: "",
       },
+      xitems: mockData,
+      xfields: fieldSpec,
     };
   },
   computed: {
+    totalRows() {
+      return this.xitems.length;
+    },
     sortOptions() {
       // Create an options list from our fields
       return this.fields
-      .filter((f) => f.sortable)
-      .map((f) => {
-        return { text: f.label, value: f.key };
-      });
+        .filter((f) => f.sortable)
+        .map((f) => {
+          return { text: f.label, value: f.key };
+        });
     },
   },
   mounted() {
